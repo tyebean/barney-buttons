0d5d6a0b064c708955c683284172c6d44b63acb9
tyebean <flower.webster@gmail.com>
2022-03-18 00:09:41 -0400

    add app.js style

--- src/App.js
+++ src/App.js
@@ -28,8 +28,6 @@ function App() {
       <CircleSelector
       selections={selections}
       />
-
-    
     </div>
   );
 }
--- src/CircleSelector/CircleSelector.jsx
+++ src/CircleSelector/CircleSelector.jsx
@@ -4,7 +4,13 @@ const CircleSelector = (props) => {
   return ( 
     <div className='circle-selector'>
       {props.selections.map((selection, idx) =>
-      <div>hey</div>
+      <button
+      key={idx}
+      className={selection ? 'selected' : ''}
+      onClick={()=>props.handleSelectCircle(idx)}
+      >
+        Button {idx+1}
+      </button>
       )}
     </div>
   );
